bb4feee83a3758bea629d63de6b892913a540304
Ryan Kretch <rkretch91@gmail.com>
2020-05-21 18:11:00 +0200

    first commit

---
+++ app/controllers/plant_tags_controller.rb
@@ -0,0 +1,36 @@
+class PlantTagsController < ApplicationController
+  def new
+    @plant = Plant.find(params[:plant_id])
+    @plant_tag = PlantTag.new
+  end
+
+  def create
+    @plant = Plant.find(params[:plant_id])
+    @tags = Tag.where(id: params[:plant_tag][:tag])
+    @tags.each do |tag|
+      plant_tag = PlantTag.new(plant: @plant, tag: tag)
+      plant_tag.save
+    end
+    redirect_to garden_path(@plant.garden)
+  end
+
+  # def create
+  #   @plant = Plant.find(params[:plant_id])
+  #   @tags = Tag.where(id: params[:plant_tag][:tag])
+  #   saved = []
+  #   @tags.each do |tag|
+  #     plant_tag = PlantTag.new
+  #     plant_tag.plant = @plant
+  #     plant_tag.tag = tag
+  #     plant_tag.save
+  #     saved << plant_tag.save
+  #   end
+  #   if saved.all? {|item| item }
+  #     redirect_to garden_path(@plant.garden)
+  #   else
+  #     @plant_tag = PlantTag.new
+  #     render :new, notice: 'dammit'
+  #   end
+  # end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     resources :plants, only: [:create]
   end
 
+  resources :plants, only: [] do
+    resources :plant_tags, only: [ :new, :create ]
+  end
+
   resources :plants, only: :destroy
 end
